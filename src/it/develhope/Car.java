48962cb39ce1504fa815ede6deccaa3c60dfb9f0
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-01-17 11:21:31 +0100

    Update Car.java

--- src/it/develhope/Car.java
+++ src/it/develhope/Car.java
@@ -1,2 +1,24 @@
-package it.develhope;public class Car {
+package it.develhope;
+
+class Car extends Vehicle {
+    public int numberOfDoors;
+    public double carPrice;
+
+    @Override
+    public void showVehicleDetails() {
+        super.showVehicleDetails();
+        System.out.println("Number of door: " + numberOfDoors);
+    }
+
+    @Override
+    public void doVehicleSound() {
+        System.out.println("Sound car");
+    }
+
+    public Car(int numberOfDoors, int numberOfWheels, double carPrice){
+        this.type = "Car";
+        this.carPrice = 20000;
+        this.numberOfDoors = 5;
+        this.numberOfWheels = 4;
+    }
 }
