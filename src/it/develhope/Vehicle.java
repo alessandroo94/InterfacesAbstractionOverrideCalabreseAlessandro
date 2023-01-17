745717588dec802527a68ba6cf20a49ddaf7de5e
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-01-17 11:22:15 +0100

    Update Vehicle.java

--- src/it/develhope/Vehicle.java
+++ src/it/develhope/Vehicle.java
@@ -1,2 +1,11 @@
-package it.develhope;public class Vehicle {
+package it.develhope;
+
+public abstract class Vehicle {
+    public String type;
+    public int numberOfWheels;
+
+    public void showVehicleDetails(){
+        System.out.println("Type: " + type + " - Numbers of wheel: " + numberOfWheels);
+    }
+    public abstract void doVehicleSound();
 }
