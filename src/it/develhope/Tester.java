4c79af28ba63e61de3af0baff829fc36f2b006a1
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-01-17 11:21:56 +0100

    Update Tester.java

--- src/it/develhope/Tester.java
+++ src/it/develhope/Tester.java
@@ -1,2 +1,18 @@
-package it.develhope;public class Tester {
+package it.develhope;
+
+public class Tester {
+    public static void main(String[] args) {
+
+        Car myCar = new Car(5, 4, 20000);
+        Boat myBoat = new Boat(35, 300);
+
+        myCar.showVehicleDetails();
+        myCar.doVehicleSound();
+        System.out.println();
+
+        myBoat.showVehicleDetails();
+        myBoat.doVehicleSound();
+        System.out.println(myBoat.getBoatWeightAndSpeed());
+
+    }
 }
