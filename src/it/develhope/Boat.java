baea1081286134c99c0c71459df58fe1b318fd2d
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-01-17 11:20:58 +0100

    Update Boat.java

--- src/it/develhope/Boat.java
+++ src/it/develhope/Boat.java
@@ -1,2 +1,21 @@
-package it.develhope;public class Boat {
+package it.develhope;
+
+class Boat extends Vehicle {
+    public double maxKnotsSpeed;
+    public int boatKilosWeight;
+
+    @Override
+    public void doVehicleSound() {
+        System.out.println("Sound Boat");
+    }
+
+    public Boat(double maxKnotsSpeed, int boatKilosWeight){
+        this.type = "Boat";
+        this.maxKnotsSpeed = 35;
+        this.boatKilosWeight = 300;
+    }
+
+    public String getBoatWeightAndSpeed(){
+        return "Weight: " + boatKilosWeight + " kg - Speed: " + maxKnotsSpeed + " knots";
+    }
 }
